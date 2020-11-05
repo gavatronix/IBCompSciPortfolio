19fa7f89d9dee5b3d4a4832fcab7128bd33622a8
gavatronix <52758843+gavatronix@users.noreply.github.com>
2020-11-05 12:38:03 -1000

    Added working DMUX

--- src/Project2/ElementaryLogicGates/Dmux.java
+++ src/Project2/ElementaryLogicGates/Dmux.java
@@ -4,7 +4,15 @@ public class Dmux {
 
     public boolean a;
     public boolean b;
+    private And and1 = new And();
+    private Not not = new Not();
+    private And and2 = new And();
 
     public void compute(boolean in, boolean sel){
+        not.compute(sel);
+        and1.compute(in,not.out);
+        a=and1.out;
+        and2.compute(in,sel);
+        b=and2.out;
     }
 }
