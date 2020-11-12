7c01fc1e60f7970780d311da90873bef9cbd7716
gavatronix <52758843+gavatronix@users.noreply.github.com>
2020-11-12 14:14:40 -0700

    Full+Half Adder

--- src/Project2/Adders/FullAdder.java
+++ src/Project2/Adders/FullAdder.java
@@ -1,15 +1,32 @@
 package Project2.Adders;
 
+import Project2.ElementaryLogicGates.And;
+import Project2.ElementaryLogicGates.Or;
+import Project2.ElementaryLogicGates.Xor;
+
 public class FullAdder {
 
     public boolean sum;
     public boolean carry;
+    private Xor[] xors = new Xor[2];
+    private And[] ands = new And[2];
+    private Or or = new Or();
 
     public FullAdder() {
+        for (int i = 0; i < xors.length; i++) {
+            xors[i] = new Xor();
+            ands[i] = new And();
+        }
     }
 
     public void compute(boolean a, boolean b, boolean c){
-
+        xors[0].compute(a,b);
+        xors[1].compute(xors[0].out,c);
+        sum = xors[1].out;
+        ands[0].compute(xors[0].out,c);
+        ands[1].compute(a,b);
+        or.compute(ands[0].out,ands[1].out);
+        carry = or.out;
 
     }
 }
--- src/Project2/Adders/HalfAdder.java
+++ src/Project2/Adders/HalfAdder.java
@@ -1,14 +1,19 @@
 package Project2.Adders;
 
+import Project2.ElementaryLogicGates.And;
+import Project2.ElementaryLogicGates.Xor;
+
 public class HalfAdder {
     public boolean sum;
     public boolean carry;
-
-    public HalfAdder() {
-    }
+    private Xor xor = new Xor();
+    private And and = new And();
 
     public void compute(boolean a, boolean b){
-
+        xor.compute(a,b);
+        and.compute(a,b);
+        sum = xor.out;
+        carry = and.out;
     }
 
 }
